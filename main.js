95d8901b895c65ab4bdcfa300df9abadc6c90acc
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-04-27 07:20:53 +0900

    implemented TODO-Button_impl

---
+++ main.js
@@ -0,0 +1,97 @@
+width = 300
+height = 300
+size = 100
+
+canvas = null
+ctx = null
+
+let field = []
+let nextfield = []
+
+const clear = () => {
+    for (let y = 0; y < size + 2; y++) {
+        field[y] = []
+        nextfield[y] = []
+        for (let x = 0; x < size + 2; x++) {
+            field[y][x] = 0
+            nextfield[y][x] = 0
+        }
+    }
+}
+
+const random = () => {
+    for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+            field[y][x] = Math.random() > 0.8 ? 1 : 0
+        }
+    }
+}
+
+const render = () => {
+    for (let y = 1; y < size+1; y++) {
+        for (let x = 1; x < size+1; x++) {
+            if (field[y][x]) {
+                ctx.fillStyle = '#0f0'
+            } else {
+                ctx.fillStyle = '#000'
+            }
+            ctx.fillRect(x * width / size, y * height / size, width / size, height / size)
+        }
+    }
+}
+
+const step = () => {
+    for (let y = 1; y <=  size; y++) {
+        for (let x = 1; x <= size; x++) {
+            let lifeCount = 0
+            for (let dy = -1; dy < 2; dy++) {
+                for (let dx = -1; dx < 2; dx++) {
+                    if (field[y + dy][x + dx]) {
+                        //console.log('survive');
+                        lifeCount++
+                    }
+                }
+            }
+            if ((field[y][x] && (lifeCount === 3 || lifeCount === 4)) ||
+                (!field[y][x] && lifeCount === 3)) {
+                //console.log('survive!!');
+                nextfield[y][x] = 1
+            } else {
+                nextfield[y][x] = 0
+            }
+        }
+    }
+    // この書き方だと，めちゃくちゃ重くなる，なぜか
+    //[field, nextfield] = [nextfield, field]
+    let buf = field
+    field = nextfield
+    nextfield = buf
+}
+
+const init = () => {
+    canvas = document.getElementById('canvas')
+    canvas.width = width
+    canvas.height = height
+    ctx = canvas.getContext('2d')
+    ctx.fillStyle = '#000'
+    ctx.fillRect(0, 0, width, height)
+
+    clear()
+    random()
+    render()
+}
+
+window.onload = async () => {
+    init()
+    // while(true){
+    //     render()
+    //     step()
+    //     await new Promise(r => setTimeout(r, 100))
+    // }
+    const tick = () => {
+        setTimeout(tick, 100)
+        step()
+        render()
+    } 
+    tick()
+}
